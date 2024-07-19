e95ab969cc5de5eec6a62ec507281bdbb7d4681f
Vaibhav Kumar Thakur <110083399+VaibhavKT20@users.noreply.github.com>
2024-07-19 22:51:45 +0530

    palindrome

---
+++ String/palindrome.java
@@ -0,0 +1,21 @@
+package String;
+
+public class palindrome {
+    public static void main(String[] args) {
+       Boolean res=checkPalindrome("NAMAN");
+        System.out.println(res);
+    }
+    public static boolean checkPalindrome(String str){
+        int start=0;
+        int end=str.length()-1;
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+
+        }
+        return true;
+    }
+}
